5cc067e58a37b3efecec378bbcf8d5e4930929cc
unknown <nadam.park@epita.fr>
2025-02-23 02:44:24 +0900

    플리 업로드, My 노트 목록 반환 api 기능 및 노트 업로드 기능 수정

--- WDYS/settings/local.py
+++ WDYS/settings/local.py
@@ -2,16 +2,16 @@
 from .base import *
 import os
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 DEBUG = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST", "localhost"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
--- uploads/urls.py
+++ uploads/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("note/", SongNoteUploadView.as_view()),  # 노트(음원)업로드
     path("note/yt/", YTNoteUploadView.as_view()),  # 노트(유튜브)업로드
     path("note/self/", NoteUploadView.as_view()),  # 노트(직접)업로드
-    # path("pli/", PliUploadView.as_view())
+    path("pli/notelist/", NoteListView.as_view()),  # 플리 생성 시 노트 목록
+    path("pli/", PliUploadView.as_view()),
 ]
