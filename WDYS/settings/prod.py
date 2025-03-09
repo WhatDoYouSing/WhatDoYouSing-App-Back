40e917fa54b84a9c1bdacc4b3aa9153c2e9dce25
julia-heo <100216331+julia-heo@users.noreply.github.com>
2025-03-10 01:38:44 +0900

    feat: 배포 파일 추가

--- WDYS/settings/prod.py
+++ WDYS/settings/prod.py
@@ -1,3 +1,17 @@
 from .base import *
 
-ALLOWED_HOSTS = ['<호스트값>']
+DEBUG = True
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'OPTIONS': {"charset": "utf8mb4",'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+    }
+}
