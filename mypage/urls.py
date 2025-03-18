608d8308205a439d9684dd6e457393e9a468f708
jieun <silver397@ewhain.net>
2025-03-18 14:10:15 +0900

    feat: 마이페이지

--- mypage/urls.py
+++ mypage/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('profile/', MyPageView.as_view()),
+    path('contents/', MyContentView.as_view()),
+    path('titles/', TitleListView.as_view()),
+    path('update/title/', TitleChoiceView.as_view()),
+    path('update/profile/', ProfileChoiceView.as_view()),
+    path('update/nickname/', NicknameUpdateView.as_view()),
+    path('calendar/', MyCalendarView.as_view()),
 ]
