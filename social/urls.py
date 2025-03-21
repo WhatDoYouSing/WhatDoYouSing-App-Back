784becf9893a94072f9e33a22ca4179f75079e9a
julia-heo <100216331+julia-heo@users.noreply.github.com>
2025-03-22 06:34:13 +0900

    feat: 팔로우 관련 api

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
+app_name = 'social'
 
 urlpatterns = [
-    
+    path('follower/',FollowerListView.as_view()),
+    path('following/',FollowingListView.as_view()),
+    path("follow/<int:user_id>/", FollowToggleView.as_view()),
 ]
