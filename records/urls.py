f4c170c6fbd6abfca7a90cafafd6824c30894417
jieun <silver397@ewhain.net>
2025-08-22 08:24:26 -0400

    chore/port

--- records/urls.py
+++ records/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('main/', MainRecordView.as_view()),
     path('emotions/', EmotionsRecordView.as_view()),
-    path("words/", WordTopView.as_view(), name="word-top"),
+    #path("words/", WordTopView.as_view(), name="word-top"),
     #path("words/<str:word>/", WordDetailView.as_view(), name="word-detail"),
 ]
