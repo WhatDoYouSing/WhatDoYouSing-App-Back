deac166b00678d42c3a62151cbd01aae912b2758
jieun <silver397@ewhain.net>
2025-08-22 08:07:09 -0400

    feat/record

--- records/apps.py
+++ records/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RecordsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'records'
+    '''
+    def ready(self):
+        import records.signals 
+    '''
